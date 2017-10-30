f61323bd9d2187a654227c767c7980ed832c3058
urroboros <gu.oleg1@gmail.com>
2017-10-30 22:49:17 +0300

    fragment who show users replace in menuActivity1

--- app/src/main/java/ru/a1024bits/bytheway/ui/activity/MenuActivity.kt
+++ app/src/main/java/ru/a1024bits/bytheway/ui/activity/MenuActivity.kt
@@ -33,28 +33,28 @@ import javax.inject.Inject
 class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnFragmentInteractionListener {
     var screenNames: ArrayList<String> = arrayListOf()
     private val STATE_SCREEN_NAMES = "state_screen_names"
-    
+
     @Inject
     lateinit var navigatorHolder: NavigatorHolder;
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         App.component.inject(this)
         setContentView(R.layout.activity_menu)
-        
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        
+
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-        
+
         val toggle = ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
-        
+
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
-        
+
         if (savedInstanceState == null) {
             navigator.applyCommand(Replace(Screens.USER_PROFILE_SCREEN, 1))
         } else {
@@ -62,8 +62,8 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
 //        startActivity(Intent(this, ShowUsersActivity::class.java))
     }
-    
-    
+
+
     override fun onBackPressed() {
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -72,7 +72,7 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
         }
     }
-    
+
     val navigator = object : SupportFragmentNavigator(supportFragmentManager, R.id.fragment_container) {
         override fun createFragment(screenKey: String?, data: Any?): Fragment {
             Log.e("LOG", screenKey + " " + data)
@@ -84,52 +84,52 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 return ShowUsersFragment.newInstance()
             return SearchFragment()
         }
-        
+
         override fun showSystemMessage(message: String?) {
             Toast.makeText(this@MenuActivity, message, Toast.LENGTH_SHORT).show();
         }
-        
+
         override fun exit() {
             finish()
         }
-        
+
         override fun applyCommand(command: Command?) {
             super.applyCommand(command)
             Log.e("LOG command", command.toString())
         }
     }
-    
+
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
         outState!!.putSerializable(STATE_SCREEN_NAMES, screenNames as java.io.Serializable)
     }
-    
+
     override fun onFragmentInteraction() {
-    
+
     }
-    
-    
+
+
     override fun onResume() {
         super.onResume()
         // App.INSTANCE.navigatorHolder.setNavigator(navigator)
         navigatorHolder.setNavigator(navigator)
     }
-    
+
     override fun onPause() {
         super.onPause()
         //  App.INSTANCE.navigatorHolder.removeNavigator()
         navigatorHolder.removeNavigator()
     }
-    
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         return true
     }
-    
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        
+
         return super.onOptionsItemSelected(item)
     }
-    
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         val id = item.itemId
@@ -141,7 +141,7 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.exit_item -> {
             }
         }
-        
+
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         drawer.closeDrawer(GravityCompat.START)
         return true
