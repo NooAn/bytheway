d75f3222f3007d399b416a25f403e6c28c5a73c3
NooAn <iamandroid1024@gmail.com>
2017-10-31 16:35:24 +0300

    add new productFlavors for mock datas

--- app/src/main/java/ru/a1024bits/bytheway/viewmodel/ShowUsersViewModel.kt
+++ app/src/main/java/ru/a1024bits/bytheway/viewmodel/ShowUsersViewModel.kt
@@ -1,6 +1,7 @@
 package ru.a1024bits.bytheway.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import ru.a1024bits.bytheway.MockWebService
 import ru.a1024bits.bytheway.model.User
 import ru.a1024bits.bytheway.repository.MockUserRepository
@@ -11,18 +12,19 @@ import java.util.*
  */
 class ShowUsersViewModel() : ViewModel() {
     var userRepo: MockUserRepository? = null
-
+    
     fun init() {
-        this.userRepo = MockUserRepository(object : MockWebService{
+        Log.e("LOG", "init");
+        this.userRepo = MockUserRepository(object : MockWebService {
             val r = Random(2000000000L)
             override fun getChanUsers(fromCount: Long, count: Int): List<User> {
                 val result: MutableList<User> = mutableListOf()
-                for (e in 1..20){
+                for (e in 1..20) {
                     result.add(User("" + r.nextLong(), "" + r.nextLong(), r.nextInt()))
                 }
                 return result
             }
         })
     }
-
+    
 }
