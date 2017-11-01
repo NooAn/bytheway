c625086b10dc08c74ef7f1fb0ed54a94a627d3c2
NooAn <iamandroid1024@gmail.com>
2017-11-01 18:24:53 +0300

    Change User Profile.

--- app/src/main/java/ru/a1024bits/bytheway/router/Screens.kt
+++ app/src/main/java/ru/a1024bits/bytheway/router/Screens.kt
@@ -6,6 +6,7 @@ package ru.a1024bits.bytheway.router
 class Screens {
     companion object {
         val USER_PROFILE_SCREEN = "user_profile_screen"
+        val MY_PROFILE_SCREEN = "user_profile_screen"
         val SEARCH_MAP_SCREEN = "start fragment screen"
         val MAIN_ACTIVITY_SCREEN = "main fragment screen"
         val ALL_USERS_SCREEN = "all_users_screen"
