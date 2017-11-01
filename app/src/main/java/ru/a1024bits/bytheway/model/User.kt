31329cea0b53a09f60a66e40a29ae5a07f3c14ad
NooAn <iamandroid1024@gmail.com>
2017-11-01 10:50:08 +0300

    add url photo in User model.

--- app/src/main/java/ru/a1024bits/bytheway/model/User.kt
+++ app/src/main/java/ru/a1024bits/bytheway/model/User.kt
@@ -7,28 +7,28 @@ import android.os.Parcelable
 /**
  * Created by andrey.gusenkov on 18/09/2017.
  */
-data class User(val name: String, val lastName: String = "", val age: Int = 0) : Parcelable{
+data class User(val name: String, val lastName: String = "", val age: Int = 0, val urlPhoto: String = "https://www.ischool.berkeley.edu/sites/default/files/default_images/avatar.jpeg") : Parcelable {
     constructor(parcel: Parcel) : this(
             parcel.readString(),
             parcel.readString(),
             parcel.readInt())
-
+    
     override fun writeToParcel(p0: Parcel, p1: Int) {
         p0.writeString(name)
         p0.writeString(lastName)
         p0.writeInt(age)
     }
-
+    
     override fun describeContents(): Int = 0
-
+    
     companion object CREATOR : Parcelable.Creator<User> {
         override fun createFromParcel(parcel: Parcel): User {
             return User(parcel)
         }
-
+        
         override fun newArray(size: Int): Array<User?> {
             return arrayOfNulls(size)
         }
     }
-
+    
 }
