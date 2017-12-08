fce8fd5e8eebd30e81974b427e2278af374d860e
urroboros <gu.oleg1@gmail.com>
2017-12-08 20:37:35 +0200

    refactor exxtensions

--- app/src/main/java/ru/a1024bits/bytheway/ExtensionsAllUsers.kt
+++ app/src/main/java/ru/a1024bits/bytheway/ExtensionsAllUsers.kt
@@ -16,15 +16,19 @@ class ExtensionsAllUsers(val context: Context) {
             toWord = " - "
         }
         val calendarStartDate = Calendar.getInstance()
-        calendarStartDate.timeInMillis = startDate!!
+        calendarStartDate.timeInMillis = startDate?: 0L
         val calendarEndDate = Calendar.getInstance()
-        calendarEndDate.timeInMillis = endDate!!
+        calendarEndDate.timeInMillis = endDate?: 0L
         var yearStart = ""
         var yearEnd = ""
         if (calendarStartDate.get(Calendar.YEAR) != calendarEndDate.get(Calendar.YEAR)) {
             yearStart = calendarStartDate.get(Calendar.YEAR).toString()
             yearEnd = calendarEndDate.get(Calendar.YEAR).toString()
         }
+        return getTextStartEndDates(fromWord, calendarStartDate, yearStart, toWord, calendarEndDate, yearEnd)
+    }
+
+    private fun getTextStartEndDates(fromWord: String?, calendarStartDate: Calendar, yearStart: String, toWord: String?, calendarEndDate: Calendar, yearEnd: String): String {
         return StringBuilder(fromWord).append(calendarStartDate.get(Calendar.DAY_OF_MONTH)).append(" ")
                 .append(context.resources.getStringArray(R.array.months_array)[calendarStartDate.get(Calendar.MONTH)]).append(" ")
                 .append(yearStart).append(toWord).append(calendarEndDate.get(Calendar.DAY_OF_MONTH)).append(" ")
