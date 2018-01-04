3924c090de3f88df3d9c56f44689d08abedef88e
al <albertincx@gmail.com>
2018-01-04 19:24:08 +0300

    REFACTORING. PROFILE.  refactor myprofileviewmodel

--- app/src/main/java/ru/a1024bits/bytheway/viewmodel/MyProfileViewModel.kt
+++ app/src/main/java/ru/a1024bits/bytheway/viewmodel/MyProfileViewModel.kt
@@ -2,13 +2,7 @@ package ru.a1024bits.bytheway.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import android.util.Log
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
 import ru.a1024bits.bytheway.model.*
-import ru.a1024bits.bytheway.repository.COLLECTION_USERS
 import ru.a1024bits.bytheway.repository.UserRepository
 import ru.a1024bits.bytheway.util.Constants.END_DATE
 import ru.a1024bits.bytheway.util.Constants.FIRST_INDEX_CITY
@@ -53,44 +47,6 @@ class MyProfileViewModel @Inject constructor(var userRepository: UserRepository)
                 .subscribe(onSuccess, { throwable ->
                     response.setValue(Response.error(throwable))
                 }))
-    }
-
-
-    fun ifUserNotExistThenSave(currentUser: FirebaseUser?) { // not used.Remove after merg!
-        val store = FirebaseFirestore.getInstance()
-        val docRef = store.collection(COLLECTION_USERS).document(currentUser?.uid.toString());
-        docRef.get().addOnCompleteListener(object : OnCompleteListener<DocumentSnapshot> {
-            override fun onComplete(task: Task<DocumentSnapshot>) {
-                if (task.isSuccessful()) {
-                    val document = task.getResult()
-                    if (!document.exists()) {
-                        // Пользователя нет в системе, добавляем.
-                        userRepository.addUser(User().apply {
-                            val list = currentUser?.displayName?.split(" ")
-                            name = list?.get(0).orEmpty()
-                            lastName = list?.get(1).orEmpty()
-                            id = currentUser?.uid.orEmpty()
-                            email = currentUser?.email.toString()
-                            phone = currentUser?.phoneNumber ?: "+7"
-                            urlPhoto = currentUser?.photoUrl.toString()
-                            currentUser?.getUid()
-                        }).addOnCompleteListener {
-                            load.value = true
-                        }.addOnFailureListener {
-                            load.value = false
-                        }
-                        Log.d("LOG", "No such document and create new doc");
-                    } else {
-                        // Пользователь уже существует и не нужно тогда добавлять его
-                        Log.d("LOG", "DocumentSnapshot data: " + task.getResult().getData());
-                        load.value = true
-                    }
-                } else {
-                    load.value = false
-                    Log.d("LOG", "get failed with ", task.getException());
-                }
-            }
-        })
     }
 
     fun sendUserData(map: HashMap<String, Any>, id: String) {
