8802a7084f24f5f4d80e77dc714a1b1fbce61223
NooAn <iamandroid1024@gmail.com>
2018-01-22 06:47:47 +0700

    add error for registration

--- app/src/main/java/ru/a1024bits/bytheway/ui/activity/RegistrationActivity.kt
+++ app/src/main/java/ru/a1024bits/bytheway/ui/activity/RegistrationActivity.kt
@@ -7,10 +7,10 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.os.Bundle
-import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -21,6 +21,7 @@ import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_splash.*
 import ru.a1024bits.bytheway.App
 import ru.a1024bits.bytheway.R
 import ru.a1024bits.bytheway.viewmodel.RegistrationViewModel
@@ -70,6 +71,7 @@ class RegistrationActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFa
                     startActivity(Intent(this@RegistrationActivity, MenuActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK))
                 } else {
                     mFirebaseAnalytics.logEvent("RegistrationScreen_Error_Checkin", null)
+                    updateUI(false)
                     Toast.makeText(this@RegistrationActivity, "Error for registration", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -108,6 +110,8 @@ class RegistrationActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFa
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result != null)
                 handleSignInResult(result)
+        } else {
+            updateUI(false)
         }
     }
 
@@ -145,6 +149,7 @@ class RegistrationActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFa
                         Toast.makeText(this, "Authentication failed.",
                                 Toast.LENGTH_SHORT).show()
                         mFirebaseAnalytics.logEvent("RegistrationScreen_Error_Login", null)
+                        updateUI(false)
                     }
                 }
     }
@@ -154,7 +159,12 @@ class RegistrationActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFa
             mFirebaseAnalytics.logEvent("RegistrationScreen_Success_Login", null)
             viewModel?.ifUserNotExistThenSave(mAuth.currentUser)
         } else {
-            Toast.makeText(this, "Not allowed Google", Toast.LENGTH_SHORT).show()
+            showErrorText()
+            Toast.makeText(this, "Проблемы с гугл аккаунтом", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun showErrorText() {
+        textError.visibility = View.VISIBLE
     }
 }
