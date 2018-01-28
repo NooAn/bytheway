ff2d12461780517d27559b9e1eaf1126aef6ffd9
NooAn <iamandroid1024@gmail.com>
2018-01-28 22:19:59 +0700

    add analitics and add right show tips

--- app/src/main/java/ru/a1024bits/bytheway/ui/dialogs/TravelSearchSaveDialog.kt
+++ app/src/main/java/ru/a1024bits/bytheway/ui/dialogs/TravelSearchSaveDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
 import ru.a1024bits.bytheway.R
 import ru.a1024bits.bytheway.ui.fragments.MapFragment
 
@@ -12,20 +13,26 @@ import ru.a1024bits.bytheway.ui.fragments.MapFragment
  */
 class TravelSearchSaveDialog(mapFragment: MapFragment) : Dialog(mapFragment.activity) {
     val fragment = mapFragment
+    protected lateinit var mFirebaseAnalytics: FirebaseAnalytics
+
     override fun dismiss() {
         super.dismiss()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(context)
         val view = layoutInflater.inflate(R.layout.d5_save_search_dialog, null)
         view.findViewById<Button>(R.id.saveButton).setOnClickListener({ v ->
             fragment.saveData()
+            mFirebaseAnalytics.logEvent("Search_screen_save", null)
+
             this.dismiss()
             this.cancel()
         })
 
         view.findViewById<Button>(R.id.cancelButton).setOnClickListener({ v ->
+            mFirebaseAnalytics.logEvent("Search_screen_save_cancel", null)
             this.cancel()
         })
 
